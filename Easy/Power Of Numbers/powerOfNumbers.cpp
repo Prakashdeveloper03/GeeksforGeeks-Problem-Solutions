a6c749df32b68bdb46da82ad26ba6aa0d3a66df1
Siva Prakash <prakashdeveloper@outlook.in>
2022-06-26 08:22:29 +0530

    Added Power Of Numbers & Smallest factorial number folder

---
+++ Easy/Power Of Numbers/powerOfNumbers.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    long long power(int N, int R)
+    {
+        long long const m = 1000000007;
+        if (R == 0)
+            return 1;
+        long long res = power(N, R / 2);
+        res = ((res % m) * (res % m)) % m;
+        if (R % 2 == 0)
+            return res;
+        return (res * (N)) % m;
+    }
+};
+
+long long rev(long long n)
+{
+    long long rev_num = 0;
+    while (n > 0)
+    {
+        rev_num = rev_num * 10 + n % 10;
+        n = n / 10;
+    }
+    return rev_num;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long int n, r;
+        cin >> n;
+        r = rev(n);
+        Solution obj;
+        cout << obj.power(n, r) << endl;
+    }
+    return 0;
+}
