b70b5c7fa230069e61cafb1916309cd76ed24194
Siva Prakash <prakashdeveloper@outlook.in>
2022-07-13 08:23:44 +0530

    Added Count Odd Factors & Perfect Numbers folders

---
+++ Easy/Count Odd Factors/countOddfactors.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int count(int N)
+    {
+        int res = abs(sqrt(N));
+        return res;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        Solution obj;
+        cout << obj.count(n) << endl;
+    }
+    return 0;
+}
