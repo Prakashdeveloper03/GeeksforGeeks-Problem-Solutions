6e151d466cd21d3deebe29d8fe4db3e25af2d5d2
Siva Prakash <prakashdeveloper@outlook.in>
2022-09-06 08:07:43 +0530

    Added Easy\C++ Exception Handling & C++ complex number addition (operator overloading) folders

---
+++ Easy/C++ Exception Handling/except.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int maxValue(int A, int B)
+    {
+        try
+        {
+            if (B == 0)
+            {
+                throw 0;
+            }
+            else
+            {
+                return max(A * B, max(A + B, max(A - B, A / B)));
+            }
+        }
+        catch (int i)
+        {
+            return -999999;
+        }
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int A, B;
+        cin >> A >> B;
+
+        Solution ob;
+        int ans = ob.maxValue(A, B);
+        if (ans == -999999)
+            cout << "inf\n";
+        else
+            cout << ans << "\n";
+    }
+    return 0;
+}
---
+++ Easy/C++ complex number addition (operator overloading)/complex.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+class complex
+{
+    int x, y;
+
+public:
+    complex() {}
+
+    complex(int real, int imag)
+    {
+        x = real;
+        y = imag;
+    }
+
+    complex operator+(complex c)
+    {
+        complex temp;
+        temp.x = x + c.x;
+        temp.y = y + c.y;
+        return temp;
+    }
+
+    void display()
+    {
+        cout << x << " + " << y << "i" << endl;
+    }
+};
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int c11, c12, c21, c22;
+        cin >> c11 >> c12 >> c21 >> c22;
+
+        complex c1, c2, c3;
+        c1 = complex(c11, c12);
+        c2 = complex(c21, c22);
+        c3 = c1 + c2;
+
+        c3.display();
+    }
+    return 0;
+}
