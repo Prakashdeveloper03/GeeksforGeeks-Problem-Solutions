8306ca3ec917b8da4417b548dd14c6560d82a88f
Siva Prakash <prakashdeveloper@outlook.in>
2022-09-20 19:47:03 +0530

    Added Basic\Count Squares & Factorial folders

---
+++ Basic/Count Squares/countSquares.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int countSquares(int N)
+    {
+        return sqrt(N - 1);
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        Solution obj;
+        cout << obj.countSquares(n) << endl;
+    }
+    return 0;
+}
