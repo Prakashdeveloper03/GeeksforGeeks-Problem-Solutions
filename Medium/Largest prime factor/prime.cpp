c6491ede60d9df873c5928d3c6f335e18dfc5cbd
Siva Prakash <prakashdeveloper@outlook.in>
2022-10-01 06:58:12 +0530

    Added Medium\Largest prime factor folder

---
+++ Medium/Largest prime factor/prime.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    bool isPrime(int n)
+    {
+        if (n <= 1)
+        {
+            return false;
+        }
+        for (int i = 2; i <= sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    long long int largestPrimeFactor(int N)
+    {
+        vector<int> factor;
+        int ans = 0;
+        if (isPrime(N) == true)
+        {
+            return N;
+        }
+        if (isPrime(N) == false)
+        {
+            for (int i = 1; i <= N / 2; i++)
+            {
+                if (N % i == 0)
+                {
+                    isPrime(i);
+                    if (isPrime(i) == true)
+                    {
+                        factor.push_back(i);
+                    }
+                }
+            }
+        }
+        ans = *max_element(factor.begin(), factor.end());
+        return ans;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.largestPrimeFactor(N) << endl;
+    }
+    return 0;
+}
