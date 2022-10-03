99037d6fa86e154f9c2b604de886c981b26cb77c
Siva Prakash <prakashdeveloper@outlook.in>
2022-10-03 10:01:09 +0530

    Added Easy\Taking input & The New Line folders

---
+++ Easy/Taking input/takeInput.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void inputData()
+{
+    int a;
+    string b;
+    cin >> a >> b;
+    cout << a << " " << b;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        inputData();
+    }
+    return 0;
+}
---
+++ Easy/The New Line/newLine.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printNewLine()
+{
+
+    cout << "Geeks" << endl
+         << "for" << endl
+         << "Geeks";
+}
+
+int main()
+{
+    printNewLine();
+    return 0;
+}
