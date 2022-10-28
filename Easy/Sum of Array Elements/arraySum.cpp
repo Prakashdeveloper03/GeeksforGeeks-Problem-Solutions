13760548cf6a39702c4cb2b77b7064ac749f1009
Siva Prakash <prakashdeveloper@outlook.in>
2022-10-28 11:00:08 +0530

    Added Easy\Sum of Array Elements folder

---
+++ Easy/Sum of Array Elements/arraySum.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int sumElement(int arr[], int n)
+{
+    if (n == 0)
+        return 0;
+    return arr[n - 1] + sumElement(arr, n - 1);
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        cout << sumElement(arr, n) << endl;
+    }
+    return 0;
+}
