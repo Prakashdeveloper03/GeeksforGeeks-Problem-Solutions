de26b38716fa1313142c28ba03fd401c2974b3b1
Siva Prakash <prakashdeveloper@outlook.in>
2022-12-16 10:47:32 +0530

    Added Easy\Two Dimensional World folder

---
+++ Easy/Two Dimensional World/2dworld.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+void twoDimensional(int **mat, int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int testcase;
+    cin >> testcase;
+    while (testcase--)
+    {
+        int N;
+        cin >> N;
+        int **mat;
+        mat = new int *[N];
+        for (int i = 0; i < N; i++)
+        {
+            mat[i] = new int[N];
+        }
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                cin >> mat[i][j];
+            }
+        }
+        twoDimensional(mat, N);
+    }
+    return 0;
+}
