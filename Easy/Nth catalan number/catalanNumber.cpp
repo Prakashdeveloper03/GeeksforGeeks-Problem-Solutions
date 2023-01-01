a2711427dc945ed115f16ad913e255655395938b
Siva Prakash <prakashdeveloper@outlook.in>
2023-01-01 14:33:49 +0530

    Added `Easy\Nth catalan number` folder

---
+++ Easy/Nth catalan number/catalanNumber.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int findCatalan(int n)
+    {
+        if (n == 0 || n == 1)
+            return 1;
+        int u, d, curr = 1;
+        for (int i = 1; i < n; i++)
+        {
+            u = 2 * i + 1;
+            u = 2 * u * curr;
+            d = i + 2;
+            curr = u / d;
+        }
+        return curr;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        Solution obj;
+        cout << obj.findCatalan(n) << "\n";
+    }
+    return 0;
+}
